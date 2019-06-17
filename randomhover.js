61aedefe74d48e82dffc519794127cf1a69a6599
gr8time <greatime111@gmail.com>
2019-06-16 20:55:19 -0400

    delete pointless js
    js graveyard clear

--- randomhover.js
+++
@@ -1,7 +0,0 @@
-
-    $('nav-trigger').hover(function() {
-        var a = Math.random() * 10 - 5;
-        $(this).css('transform', 'rotate(' + a + 'deg) scale(1.25)');
-    }, function() {
-        $(this).css('transform', 'none');
-    });
