6572485c39dccb8cc61f9ef4ac3fd1b822215347
gr8time <greatime111@gmail.com>
2019-07-07 13:32:02 -0400

    hoverPop attempt
    hopefully didnt break tour hoverPop

--- hoverPop.js
+++ hoverPop.js
@@ -1,3 +1,7 @@
+/* var navLi = document.getElementById("nav-list").getElementsByTagName("li");
+var sound = document.getElementById(soundobj);
+navLi.onmouseover = PlaySound('pop'); */
+
 function PlaySound(soundobj) {
     var thissound=document.getElementById(soundobj);
     thissound.play();
