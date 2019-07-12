9db65312cbbe694dde461c30825492d655575f19
gr8time <greatime111@gmail.com>
2019-07-12 18:00:49 -0400

    restructure files
    only html in root now

--- scripts/click.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementById("clickme"),
-  count = 0;
-button.onclick = function() {
-	
-  count += 1000;
-  count *= 37;
-  button.innerHTML = "POWER METER: " + count;
-};
--- scripts/playsound.js
+++
@@ -1,5 +0,0 @@
-function playSound(soundfile) {
-document.getElementById("dummy").innerHTML= "<embed src=\""
-+soundfile+"\" hidden=\"true\" autostart=\"true\"
-loop=\"false\" />";
-}
