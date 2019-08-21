8ec74bb42b29ab52c1df54b351669cb1b2883480
gr8time <greatime111@gmail.com>
2019-08-20 23:39:53 -0400

    pressQuotes animation time
    25s to 19s

--- scripts/pressQuotes.js
+++ scripts/pressQuotes.js
@@ -40,7 +40,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1; }
   slides[slideIndex - 1].style.display = 'block';
-  setTimeout(showSlides, 25000);
+  setTimeout(showSlides, 19000);
 }
 
 renderFacts();
